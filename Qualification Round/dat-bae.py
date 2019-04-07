39fc2dd98f0d9c2660ba3e0d30738bb898e84fb9
kamyu <kamyu104@users.noreply.github.com>
2019-04-07 18:22:24 +0800

    Update dat-bae.py

--- Qualification Round/dat-bae.py
+++ Qualification Round/dat-bae.py
@@ -14,7 +14,7 @@ def encode(query, i, flip, total, valid):
     query.append(str(flip)*total)
     return valid, i
 
-def decode(response, next_segments, i, flip, total, valid):
+def decode(response, next_blocks, i, flip, total, valid):
     def count(response, i, c, cnt):
         same_cnt = 0
         while i < len(response) and same_cnt < cnt:
@@ -26,11 +26,11 @@ def decode(response, next_segments, i, flip, total, valid):
         return same_cnt, i
     
     used_valid, i = count(response, i, str(flip), valid)
-    next_segments.append((total, used_valid))
+    next_blocks.append((total, used_valid))
     return used_valid, i
 
 def init_codec(N, total, callback):
-    # split N into segments with size "total"
+    # split N into blocks with size "total"
     i = 0
     cnt, flip = N, 0
     while cnt > total:
@@ -39,16 +39,16 @@ def init_codec(N, total, callback):
         flip ^= 1
     used_valid, i = callback(i, flip, cnt, cnt)
 
-def codec(segments, callback):
+def codec(blocks, callback):
     i = 0
     is_done = True
-    for total, valid in segments:
+    for total, valid in blocks:
         if total == valid or valid == 0:
             used_valid, i = callback(i, 0, total, valid)
         else:
-            # equally split each segment into 2 segments.
+            # equally split each block into 2 blocks.
             # after ceil(log2(B)) times splits,
-            # each segment must converge into size 1 or stop split
+            # each block must converge into size 1 or stop split
             is_done = False
             used_valid, i = callback(i, 0, total//2, valid)
             used_valid, i = callback(i, 1, (total+1)//2, valid-used_valid)
@@ -66,32 +66,32 @@ def dat_bae():
     while size < B:
         size *= 2
     
-    segments = [] if size < N else [(N, N-B)]
+    blocks = [] if size < N else [(N, N-B)]
     while size:  # min(ceil(log2(N-1)), ceil(log2(B)) + 1) times
         query = []
         query_callback = functools.partial(encode, query)
-        if not segments:
+        if not blocks:
             init_codec(N, size, query_callback)
         else:
-            is_done = codec(segments, query_callback)
+            is_done = codec(blocks, query_callback)
             if is_done: break
 
         print "".join(query)
         sys.stdout.flush()
         response = list(raw_input().strip().split()[0])
 
-        next_segments = []
-        response_callback = functools.partial(decode, response, next_segments)
-        if not segments:
+        next_blocks = []
+        response_callback = functools.partial(decode, response, next_blocks)
+        if not blocks:
             init_codec(N, size, response_callback)
         else: 
-            codec(segments, response_callback)
-        segments, next_segments = next_segments, segments
+            codec(blocks, response_callback)
+        blocks, next_blocks = next_blocks, blocks
 
         size //= 2
 
     result, i = [], 0
-    for total, valid in segments:
+    for total, valid in blocks:
         if valid == 0:
             for j in xrange(i, i+total):
                 result.append(str(j))
