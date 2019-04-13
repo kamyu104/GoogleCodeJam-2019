a8830bd246dd49493a8e83733f52316ed101b03b
kamyu <kamyu104@users.noreply.github.com>
2019-04-13 22:05:08 +0800

    Update golf-gophers2.py

--- Round 1A/golf-gophers2.py
+++ Round 1A/golf-gophers2.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2019 Round 1A - Problem B. Golf Gophers
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000051635/0000000000104e05
 #
-# Time:  O(B * ((logM/logK) + M)), K is the min of k s.t. P(M, k) >= M
+# Time:  O(B * (N + M))
 # Space: O(B)
 #
 
@@ -12,18 +12,15 @@ import sys
 B = 18
 def golf_gophers(N, M):
     modulis, residues = [], []
-    cnt = 1
     for i in reversed(xrange(max(2, B-N+1), B+1)):
         print " ".join(map(str, [i]*B))
         sys.stdout.flush()
         modulis.append(i)
         residues.append(sum(map(int, raw_input().strip().split())) % i)
-        cnt *= i
-        if i > M:
-            break
 
     # these modulis won't work in chinese remainder theorem,
-    # but if the modulis are [18, 17, 16, 15, 14], the residues of m are unique if m <= 1000000
+    # but if the modulis are [18, 17, 16, 15, 14, 13, 12], the residues of m are unique if m <= 1113840
+    # see golf-gophers2-prove.py
     for m in xrange(1, M+1):
         for i, residue in enumerate(residues):
             if m % modulis[i] != residue:
