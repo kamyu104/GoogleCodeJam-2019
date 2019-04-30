e044cddfb57e61d52c9db0eb657b7c09fecf88b6
kamyu <kamyu104@users.noreply.github.com>
2019-04-30 11:43:24 +0800

    Update fair-fight.py

--- Round 1B/fair-fight.py
+++ Round 1B/fair-fight.py
@@ -8,26 +8,25 @@
 #
 
 import collections
-import math
+import itertools
 
-class RMQ(object):
-    def __init__(self, nums):
-        n = 1
-        while 2**n <= len(nums):
-            n += 1
-        n -= 1
-        self.__dp = [[0 for _ in xrange(n+1)]for _ in xrange(len(nums))]
-        for i in xrange(len(nums)):
-            self.__dp[i][0] = nums[i]
-        j = 1
-        while 2**j <= len(nums):
-            for i in xrange(len(nums)+1-2**j):
-                self.__dp[i][j] = max(self.__dp[i][j-1], self.__dp[i+2**(j-1)][j-1])
-            j += 1
+class RangeQuery(object):
+    def __init__(self, items, fn):
+        self._rq = rq = {(i, 0): item for i, item in enumerate(items)}
+        self._fn = fn
+        n = len(items)
+        for step, i in itertools.product(xrange(1, n.bit_length()), xrange(n)):
+            j = i + 2 ** (step-1)
+            if j < n:
+                rq[i, step] = fn(rq[i, step-1], rq[j, step-1])
+            else:
+                rq[i, step] = rq[i, step-1]
 
-    def query(self, i, j):
-        k = int(math.log(j-i+1)/math.log(2))
-        return max(self.__dp[i][k], self.__dp[j-2**k+1][k])
+    def query(self, start, stop):
+        j = (stop - start).bit_length() - 1
+        x = self._rq[start, j]
+        y = self._rq[stop - 2 ** j, j]
+        return self._fn(x, y)
 
 def lower_bound(left, right, check):
     while left <= right:
@@ -51,17 +50,17 @@ def fair_fight():
     N, K = map(int, raw_input().strip().split())
     C = map(int, raw_input().strip().split())
     D = map(int, raw_input().strip().split())
-    RMQ_C, RMQ_D = RMQ(C), RMQ(D)
+    RMQ_C, RMQ_D = RangeQuery(C, max), RangeQuery(D, max)
     result, next_to_last_seen = 0, collections.defaultdict(int)
     for i, Ci in enumerate(C):
         L_good = lower_bound(next_to_last_seen[Ci], i,
-                             lambda x: RMQ_C.query(x, i) == Ci and RMQ_D.query(x, i)-Ci <= K)
+                             lambda x: RMQ_C.query(x, i+1) == Ci and RMQ_D.query(x, i+1)-Ci <= K)
         R_good = upper_bound(i, N-1,
-                             lambda x: RMQ_C.query(i, x) == Ci and RMQ_D.query(i, x)-Ci <= K)-1
+                             lambda x: RMQ_C.query(i, x+1) == Ci and RMQ_D.query(i, x+1)-Ci <= K)-1
         L_bad = lower_bound(L_good, i,
-                            lambda x: RMQ_C.query(x, i) == Ci and RMQ_D.query(x, i)-Ci <= -K-1)
+                            lambda x: RMQ_C.query(x, i+1) == Ci and RMQ_D.query(x, i+1)-Ci <= -K-1)
         R_bad = upper_bound(i, R_good,
-                            lambda x: RMQ_C.query(i, x) == Ci and RMQ_D.query(i, x)-Ci <= -K-1)-1
+                            lambda x: RMQ_C.query(i, x+1) == Ci and RMQ_D.query(i, x+1)-Ci <= -K-1)-1
         result += (i-L_good+1)*(R_good-i+1)-(i-L_bad+1)*(R_bad-i+1)
         next_to_last_seen[Ci] = i+1  # to avoid duplicated count
     return result
