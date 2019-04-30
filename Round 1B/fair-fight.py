d82af6dbc0026b12e407740758226d12faa17cd9
kamyu <kamyu104@users.noreply.github.com>
2019-05-01 00:16:08 +0800

    Update fair-fight.py

--- Round 1B/fair-fight.py
+++ Round 1B/fair-fight.py
@@ -9,11 +9,11 @@
 
 import collections
 
-def lower_bound(A, curr_maxs, K):
-    left, right = 0, len(curr_maxs)-1
+def lower_bound(A, curr_max_idxs, K):
+    left, right = 0, len(curr_max_idxs)-1
     while left <= right:
         mid = (left + right) // 2
-        if A[curr_maxs[mid]] <= K:
+        if A[curr_max_idxs[mid]] <= K:
             right = mid-1
         else:
             left = mid+1
@@ -25,46 +25,46 @@ def fair_fight():
     D = map(int, raw_input().strip().split())
 
     L_lookup = collections.defaultdict(int)
-    C_curr_maxs, D_curr_maxs = [], []
+    C_curr_max_idxs, D_curr_max_idxs = [], []
     for i in xrange(N):
-        while C_curr_maxs and C[C_curr_maxs[-1]] < C[i]:  # keep the idx where C[idx] == Ci
-            C_curr_maxs.pop()
-        C_L_idx = C_curr_maxs[-1]+1 if C_curr_maxs else 0  # get the leftmost idx of Ci s.t. C[idx] < Ci
-        C_curr_maxs.append(i)
-        while D_curr_maxs and D[D_curr_maxs[-1]] <= D[i]:
-            D_curr_maxs.pop()
-        D_curr_maxs.append(i)
+        while C_curr_max_idxs and C[C_curr_max_idxs[-1]] < C[i]:  # keep the idx where C[idx] == Ci
+            C_curr_max_idxs.pop()
+        C_L_idx = C_curr_max_idxs[-1]+1 if C_curr_max_idxs else 0  # get the leftmost idx of Ci s.t. C[idx] < Ci
+        C_curr_max_idxs.append(i)
+        while D_curr_max_idxs and D[D_curr_max_idxs[-1]] <= D[i]:
+            D_curr_max_idxs.pop()
+        D_curr_max_idxs.append(i)
 
         if D[i]-C[i] > K:
             continue
         
-        D_L_good = lower_bound(D, D_curr_maxs, C[i]+K)
-        D_L_bad = lower_bound(D, D_curr_maxs, C[i]-K-1)
-        D_L_good_idx = D_curr_maxs[D_L_good-1]+1 if D_L_good > 0 else 0
-        D_L_bad_idx = D_curr_maxs[D_L_bad-1]+1 if D_L_bad > 0 else 0
+        D_L_good = lower_bound(D, D_curr_max_idxs, C[i]+K)
+        D_L_bad = lower_bound(D, D_curr_max_idxs, C[i]-K-1)
+        D_L_good_idx = D_curr_max_idxs[D_L_good-1]+1 if D_L_good > 0 else 0
+        D_L_bad_idx = D_curr_max_idxs[D_L_bad-1]+1 if D_L_bad > 0 else 0
         L_good = max(C_L_idx, D_L_good_idx)
         L_bad = max(L_good, D_L_bad_idx)
 
         L_lookup[i] = (L_good, L_bad)
 
     result = 0
-    C_curr_maxs, D_curr_maxs = [], []
+    C_curr_max_idxs, D_curr_max_idxs = [], []
     for i in reversed(xrange(N)):
-        while C_curr_maxs and C[C_curr_maxs[-1]] <= C[i]:
-            C_curr_maxs.pop()
-        C_R_idx = C_curr_maxs[-1]-1 if C_curr_maxs else N-1  # get the rightmost idx of Ci s.t. Ci >= C[idx]
-        C_curr_maxs.append(i)
-        while D_curr_maxs and D[D_curr_maxs[-1]] <= D[i]:
-            D_curr_maxs.pop()
-        D_curr_maxs.append(i)
+        while C_curr_max_idxs and C[C_curr_max_idxs[-1]] <= C[i]:
+            C_curr_max_idxs.pop()
+        C_R_idx = C_curr_max_idxs[-1]-1 if C_curr_max_idxs else N-1  # get the rightmost idx of Ci s.t. Ci >= C[idx]
+        C_curr_max_idxs.append(i)
+        while D_curr_max_idxs and D[D_curr_max_idxs[-1]] <= D[i]:
+            D_curr_max_idxs.pop()
+        D_curr_max_idxs.append(i)
 
         if D[i]-C[i] > K:
             continue
 
-        D_R_good = lower_bound(D, D_curr_maxs, C[i]+K)
-        D_R_bad = lower_bound(D, D_curr_maxs, C[i]-K-1)
-        D_R_good_idx = D_curr_maxs[D_R_good-1]-1 if D_R_good > 0 else N-1
-        D_R_bad_idx = D_curr_maxs[D_R_bad-1]-1 if D_R_bad > 0 else N-1
+        D_R_good = lower_bound(D, D_curr_max_idxs, C[i]+K)
+        D_R_bad = lower_bound(D, D_curr_max_idxs, C[i]-K-1)
+        D_R_good_idx = D_curr_max_idxs[D_R_good-1]-1 if D_R_good > 0 else N-1
+        D_R_bad_idx = D_curr_max_idxs[D_R_bad-1]-1 if D_R_bad > 0 else N-1
         R_good = min(C_R_idx, D_R_good_idx)
         R_bad = min(R_good, D_R_bad_idx)
 
