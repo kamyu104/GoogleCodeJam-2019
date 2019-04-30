5e92bd62636aff9b4895265ed91467c7cf38d5f7
kamyu <kamyu104@users.noreply.github.com>
2019-05-01 00:35:49 +0800

    Update fair-fight.py

--- Round 1B/fair-fight.py
+++ Round 1B/fair-fight.py
@@ -40,8 +40,8 @@ def fair_fight():
         C_L = C_curr_max_idxs[-2]+1 if len(C_curr_max_idxs) >= 2 else 0  # get the leftmost idx of Ci s.t. C[idx] < Ci
         D_L_good_it = lower_bound(D, D_curr_max_idxs, C[i]+K)
         D_L_bad_it = lower_bound(D, D_curr_max_idxs, C[i]-K-1)
-        D_L_good = D_curr_max_idxs[D_L_good_it-1]+1 if D_L_good_it > 0 else 0
-        D_L_bad = D_curr_max_idxs[D_L_bad_it-1]+1 if D_L_bad_it > 0 else 0
+        D_L_good = D_curr_max_idxs[D_L_good_it-1]+1 if D_L_good_it > 0 else 0  # get the leftmost idx of max_D s.t. max_D-Ci <= K
+        D_L_bad = D_curr_max_idxs[D_L_bad_it-1]+1 if D_L_bad_it > 0 else 0  # get the leftmost idx of max_D s.t. max_D-Ci <= -K-1
         L_good = max(C_L, D_L_good)
         L_bad = max(L_good, D_L_bad)
 
@@ -63,8 +63,8 @@ def fair_fight():
         C_R = C_curr_max_idxs[-2]-1 if len(C_curr_max_idxs) >= 2 else N-1  # get the rightmost idx of Ci s.t. Ci >= C[idx]
         D_R_good_it = lower_bound(D, D_curr_max_idxs, C[i]+K)
         D_R_bad_it = lower_bound(D, D_curr_max_idxs, C[i]-K-1)
-        D_R_good = D_curr_max_idxs[D_R_good_it-1]-1 if D_R_good_it > 0 else N-1
-        D_R_bad = D_curr_max_idxs[D_R_bad_it-1]-1 if D_R_bad_it > 0 else N-1
+        D_R_good = D_curr_max_idxs[D_R_good_it-1]-1 if D_R_good_it > 0 else N-1  # get the rightmost idx of max_D s.t. max_D-Ci <= K
+        D_R_bad = D_curr_max_idxs[D_R_bad_it-1]-1 if D_R_bad_it > 0 else N-1  # get the rightmost idx of max_D s.t. max_D-Ci <= -K-1
         R_good = min(C_R, D_R_good)
         R_bad = min(R_good, D_R_bad)
 
