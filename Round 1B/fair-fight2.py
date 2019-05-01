0e85c08f2f499718fb3d0e9b76bfc9f6b978880f
kamyu <kamyu104@users.noreply.github.com>
2019-05-01 15:54:38 +0800

    Update fair-fight2.py

--- Round 1B/fair-fight2.py
+++ Round 1B/fair-fight2.py
@@ -53,17 +53,17 @@ def fair_fight():
     N, K = map(int, raw_input().strip().split())
     C = map(int, raw_input().strip().split())
     D = map(int, raw_input().strip().split())
-    RMQ_C, RMQ_D = RangeQuery(C, max), RangeQuery(D, max)
+    C_RMQ, D_RMQ = RangeQuery(C, max), RangeQuery(D, max)
     result, next_to_last_seen = 0, collections.defaultdict(int)
     for i, Ci in enumerate(C):
         L_good = lower_bound(next_to_last_seen[Ci], i,
-                             lambda x: RMQ_C.query(x, i+1) == Ci and RMQ_D.query(x, i+1)-Ci <= K)
+                             lambda x: C_RMQ.query(x, i+1) == Ci and D_RMQ.query(x, i+1)-Ci <= K)
         R_good = upper_bound(i, N-1,
-                             lambda x: RMQ_C.query(i, x+1) == Ci and RMQ_D.query(i, x+1)-Ci <= K)-1
+                             lambda x: C_RMQ.query(i, x+1) == Ci and D_RMQ.query(i, x+1)-Ci <= K)-1
         L_bad = lower_bound(L_good, i,
-                            lambda x: RMQ_C.query(x, i+1) == Ci and RMQ_D.query(x, i+1)-Ci <= -K-1)
+                            lambda x: C_RMQ.query(x, i+1) == Ci and D_RMQ.query(x, i+1)-Ci <= -K-1)
         R_bad = upper_bound(i, R_good,
-                            lambda x: RMQ_C.query(i, x+1) == Ci and RMQ_D.query(i, x+1)-Ci <= -K-1)-1
+                            lambda x: C_RMQ.query(i, x+1) == Ci and D_RMQ.query(i, x+1)-Ci <= -K-1)-1
         result += (i-L_good+1)*(R_good-i+1)-(i-L_bad+1)*(R_bad-i+1)
         next_to_last_seen[Ci] = i+1  # to avoid duplicated count
     return result
