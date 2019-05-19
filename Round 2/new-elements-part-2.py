ae1f40e4d3f447955fe1e36569f2f45474044e80
Allen Liu <kamyu104@gmail.com>
2019-05-19 23:27:51 +0800

    update

--- Round 2/new-elements-part-2.py
+++ Round 2/new-elements-part-2.py
@@ -14,8 +14,8 @@ def find_fraction_with_min_denominator_between(a, b, d=0):
     assert(d < 9)
     if b-int(a) > 1:
         return Fraction(int(a)+1, 1)
-    if a == int(a):
-        return a + Fraction(1, int(1/(b-a))+1)
+    if a-int(a) == 0:
+        return int(a) + Fraction(1, int(1/(b-int(a)))+1)
     return int(a) + 1/find_fraction_with_min_denominator_between(1/(b-int(a)), 1/(a-int(a)), d+1)
 
 def new_elements_part_2():
@@ -24,23 +24,22 @@ def new_elements_part_2():
     for _ in xrange(N):
         molecules.append(map(int, raw_input().strip().split()))
 
-    lowers, uppers = set([Fraction(0, 1)]), set([Fraction(MAX_C_J, 1)])
+    L, U = Fraction(0, 1), Fraction(MAX_C_J, 1)
     for b in xrange(1, N):
         (Cb, Jb) = molecules[b]
         for a in xrange(b):
             (Ca, Ja) = molecules[a]
             if (Ca < Cb and Ja > Jb):
-                uppers.add(Fraction(Ca-Cb, Jb-Ja))
+                U = min(U,  Fraction(Ca-Cb, Jb-Ja))
             elif (Ca > Cb and Ja < Jb):
-                lowers.add(Fraction(Ca-Cb, Jb-Ja))
+                L = max(L, Fraction(Ca-Cb, Jb-Ja))
             elif (Ca >= Cb) and (Ja >= Jb):
                 return "IMPOSSIBLE"
 
-    lower, upper = max(lowers), min(uppers)
-    if lower >= upper:
+    if L >= U:
         return "IMPOSSIBLE"
-    cf = find_fraction_with_min_denominator_between(lower, upper)
-    return "{} {}".format(cf.denominator, cf.numerator)
+    frac = find_fraction_with_min_denominator_between(L, U)
+    return "{} {}".format(frac.denominator, frac.numerator)
 
 MAX_C_J = 10**9
 for case in xrange(input()):
