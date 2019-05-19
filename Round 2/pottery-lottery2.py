3bbe75797066bb55b2d400c07586408e56ef6f63
kamyu <kamyu104@users.noreply.github.com>
2019-05-20 07:50:07 +0800

    Update pottery-lottery2.py

--- Round 2/pottery-lottery2.py
+++ Round 2/pottery-lottery2.py
@@ -10,39 +10,34 @@
 from sys import stdout
 from heapq import heappush, heappop
 
+def put(i, v, D):
+    _ = input()
+    D[0] -= 1
+    print i+1, v
+    stdout.flush()
+
 def pottery_lottery():
-    D = P-1
+    D = [P]
 
     # day 1 ~ N
     for i in xrange(N):  # sabotage
-        _ = input()
-        D -= 1
-        print (i % S)+1, 1
-        stdout.flush()
+        put(i%S, 1, D)
 
     # day N+1 ~ N+V
     min_heap = []
     for i in xrange(V):  # inspect
-        _ = input()
-        D -= 1
-        print i+1, 0
-        stdout.flush()
+        put(i, 0, D)
         heappush(min_heap, (len(raw_input().strip().split()), -i))
     
     # day N+V+1 ~ P-1
-    candidate = heappop(min_heap)[1]
-    while D:  # sabotage
-        _ = input()
-        D -= 1
+    candidate = -heappop(min_heap)[1]
+    while D[0] > 1:  # sabotage
         count, i = heappop(min_heap)
-        print -i+1, 1
-        stdout.flush()
+        put(-i, 1, D)
         heappush(min_heap, (count+1, i))
  
     # day P
-    _ = input()
-    print -candidate+1, P
-    stdout.flush()
+    put(candidate, P, D)
 
 P, V = 100, 20
 N, S = 60, 14  # tuned by testing_tool.py (243/250)
