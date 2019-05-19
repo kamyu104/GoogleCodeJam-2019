f8a92de0edc9b9e9ffe090fb2f6c38db01c5e310
kamyu <kamyu104@users.noreply.github.com>
2019-05-20 07:44:12 +0800

    Update pottery-lottery.py

--- Round 2/pottery-lottery.py
+++ Round 2/pottery-lottery.py
@@ -10,60 +10,49 @@
 from sys import stdout
 from heapq import heappush, heappop
 
+def put(i, v, D):
+    _ = input()
+    D[0] -= 1
+    print i+1, v
+    stdout.flush()
+
 def pottery_lottery():
-    D = P-1
+    D = [P]
 
     # day 1 ~ N1
     for i in xrange(N1):  # sabotage
-        _ = input()
-        D -= 1
-        print (i % S)+1, 1
-        stdout.flush()
+        put(i%S, 1, D)
 
     # day N1+1 ~ N1+V
     min_heap = []
     for i in xrange(V):  # inspect
-        _ = input()
-        D -= 1
-        print i+1, 0
-        stdout.flush()
+        put(i, 0, D)
         heappush(min_heap, (len(raw_input().strip().split()), -i))
     
     # day N1+V+1 ~ N1+V+N2
     candidates = []
     for _ in xrange(C):
-        candidates.append(heappop(min_heap)[1])
+        candidates.append(-heappop(min_heap)[1])
     for _ in xrange(N2):  # sabotage
-        _ = input()
-        D -= 1
         count, i = heappop(min_heap)
-        print -i+1, 1
-        stdout.flush()
+        put(-i, 1, D)
         heappush(min_heap, (count+1, i))
 
     # day N1+V+N2+1 ~ N1+V+N2+C
     min_heap = []
     for i in candidates:  # inspect
-        _ = input()
-        D -= 1
-        print -i+1, 0
-        stdout.flush()
+        put(i, 0, D)
         heappush(min_heap, (len(raw_input().strip().split()), i))
 
     # day N1+V+N2+C+1 ~ P-1
-    candidate = heappop(min_heap)[1]
-    while D:  # sabotage
-        _ = input()
-        D -= 1
+    candidate = -heappop(min_heap)[1]
+    while D[0] > 1:  # sabotage
         count, i = heappop(min_heap)
-        print -i+1, 1
-        stdout.flush()
+        put(-i, 1, D)
         heappush(min_heap, (count+1, i))
  
     # day P
-    _ = input()
-    print -candidate+1, P
-    stdout.flush()
+    put(candidate, P, D)
 
 P, V = 100, 20
 N1, S, N2, C = 60, 14, 14, 2  # tuned by testing_tool.py (247/250)
