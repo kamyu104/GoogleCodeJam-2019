8c196c4dbf1f7e6533c6badd5f6edb24ce2633a7
Allen Liu <kamyu104@gmail.com>
2019-05-19 12:18:38 +0800

    update

---
+++ Round 2/new-elements-part-1.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2019 kamyu. All rights reserved.
+#
+# Google Code Jam 2019 Round 2 - Problem A. New Elements: Part 1
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000051679/0000000000146183
+#
+# Time:  O(N^2)
+# Space: O(N^2)
+#
+
+from fractions import Fraction
+
+def new_elements_part_1():
+    N = input()
+    molecules = []
+    for _ in xrange(N):
+        molecules.append(map(int, raw_input().strip().split()))
+    fractions_set = set()
+    for Ca, Ja in molecules:
+        for Cb, Jb in molecules:
+            if Ca < Cb and Ja > Jb:
+                fractions_set.add(Fraction(Ca-Cb, Jb-Ja))
+    return len(fractions_set)+1
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, new_elements_part_1())
