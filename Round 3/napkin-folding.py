36573698bf0bb1798ed6ac24170087033c5187d6
Allen Liu <kamyu104@gmail.com>
2019-06-11 01:29:15 +0800

    update

--- Round 3/napkin-folding.py
+++ Round 3/napkin-folding.py
@@ -19,13 +19,6 @@ def polygon_area(polygon):
         area += advance_polygon_area(polygon[i-1], polygon[i])
     return abs(area/2)
 
-# Return true if line segments AB and CD intersect
-def intersect(A, B, C, D):
-    def ccw(A, B, C):
-        return (C[1]-A[1]) * (B[0]-A[0]) - (B[1]-A[1]) * (C[0]-A[0]) > 0
-
-    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
-
 # Return Q which is the reflection of P over line (A, B)
 def reflect(P, A, B):
     a, b, c = A[1]-B[1], -(A[0]-B[0]), (A[1]-B[1])*(-A[0])-(A[0]-B[0])*(-A[1])
@@ -69,65 +62,44 @@ def find_possible_pairs(polygon, K, endpoints):
                 yield (start, i)
             i = (i+1)%len(endpoints)
 
-class Vertex(object):
-    def __init__(self, begin, end, length, C):
-        self.val = begin
-        self.begin = begin
-        self.end = end
-        self.length = length
-        self.C = C
-
-    def next(self):
-        if self.val == self.end:
-            return self.begin
-        result = self.val
-        if result > self.end:
-            result -= self.length
-        if result + self.C < self.end:
-            result = ((result+self.C)%self.length)//self.C*self.C
-        else:
-            result = self.end
-        return result
-
-def left_right(A, length, C):
-    if A % C == 0:
-        return (A, A)
-    return (A//C*C, ((A-1)//C+1)*C%length)
+def find_pattern(begin, end, length, C):
+    pattern = [begin]
+    while C < abs(pattern[-1]-end) < length-C:
+        pattern.append(((pattern[-1]+C)%length)//C*C)
+    pattern.append(end)
+    return pattern
 
 def is_on_polygon_edge(A, B, length, C):
+    def left_right(A, length, C):
+        if A % C == 0:
+            return (A, A)
+        return (A//C*C, ((A-1)//C+1)*C%length)
+
     A_left, A_right = left_right(A, length, C)
     B_left, B_right = left_right(B, length, C)
     if A_left == A_right and B_left == B_right:
-        return abs(A_left-B_left) <= C or abs(A_left-B_left) >= length-C
+        return not (C < abs(A_left-B_left) < length-C)
     if A_left != A_right:
         return B_left in (A_left, A_right)
-    if B_left != B_right:
-        return A_left in (B_left, B_right)
-    return (A_left, A_right) == (B_left, B_right)
+    return A_left in (B_left, B_right)
 
 def normalize(a, b):
     return (a, b) if a <= b else (b, a)
 
 def find_valid_pairs(polygon, K, endpoints, endpoints_idx, pair):
-    for i in xrange(len(polygon)):
-        if intersect(polygon[i-1], polygon[i], endpoints[pair[0]], endpoints[pair[1]]):
-            return None
-    C = len(endpoints)//len(polygon)
-    polygon_set = set(polygon)
+    C = len(endpoints)//len(polygon)  # count of polygon and non-polygon vertex on an edge
 
-    v = Vertex(pair[0], pair[1], len(endpoints), C)
-    pattern = [v.val]
-    v.val = v.next()
-    while v.val != v.end:
-        pattern.append(v.val)
-        polygon_set.discard(endpoints[v.val])
-        v.val = v.next()
-    pattern.append(v.val)
+    polygon_set = set(polygon)
+    pattern = find_pattern(pair[0], pair[1], len(endpoints), C)
+    for p in pattern:
+        polygon_set.discard(endpoints[p])
+
     pairs = set()
     q = deque([(pair, pattern)])
     while len(pairs) != K-1 and q:
         (pair, pattern) = q.popleft()
         pairs.add(normalize(pair[0], pair[1]))
+
         new_pairs, new_pattern = [], []
         for i in xrange(-1, len(pattern)):
             p = reflect(endpoints[pattern[i]], endpoints[pair[0]], endpoints[pair[1]])
@@ -141,13 +113,16 @@ def find_valid_pairs(polygon, K, endpoints, endpoints_idx, pair):
                     if new_pair not in pairs:
                         new_pairs.append(new_pair)
             new_pattern.append(p_idx)
+
         for new_pair in new_pairs:
             q.append((new_pair, new_pattern))
-    return list(pairs) if not polygon_set and len(pairs) == K-1 and not q else None
+
+    return pairs if not polygon_set and len(pairs) == K-1 and not q else None
 
 def output(p):
     return "{}/{} {}/{}".format(p[0].numerator, p[0].denominator,
                                 p[1].numerator, p[1].denominator)
+
 def napkin_folding():
     N, K = map(int, raw_input().strip().split())
     polygon = []
