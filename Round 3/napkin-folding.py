0c5ee329179d95b925afc20089831271cc85ebe3
kamyu <kamyu104@users.noreply.github.com>
2019-07-08 11:21:58 +0800

    Update napkin-folding.py

--- Round 3/napkin-folding.py
+++ Round 3/napkin-folding.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2019 Round C - Problem D. Napkin Folding
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000051707/0000000000159170
 #
-# Time:  O(N^2 * K^3), pass in PyPy2 but Python2 for set 2
+# Time:  O(N^2 * K^3)
 # Space: O(N * K^2)
 #
 
