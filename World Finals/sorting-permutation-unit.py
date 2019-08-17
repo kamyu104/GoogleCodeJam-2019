c371d03aac7bf49881a146a4ab5e3b13828234c0
Allen Liu <kamyu104@gmail.com>
2019-08-18 01:23:42 +0800

    update

--- World Finals/sorting-permutation-unit.py
+++ World Finals/sorting-permutation-unit.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2019 World Finals - Problem B. Sorting Permutation Unit
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000051708/000000000016c77d
 #
-# Time:  O(K * N^2), each array costs 1.5N + 4N + N = 8.5N operations
+# Time:  O(K * N^2), each array costs 1.5N + 6N + N = 8.5N operations
 # Space: O(N)
 #
 
@@ -29,7 +29,7 @@ def rotate_and_add_seq(nums, k, seq, shift):
     assert(k >= 0)  # k should be non-negative rotation count to avoid wrong permutations
     shift[0] = (shift[0]+k)%(len(nums)-1)
     rotate(nums, k, len(nums)-1)
-    seq.extend(ROTATIONS[k])  # split k rotations into at most 4 permutations
+    seq.extend(ROTATIONS[k])  # split k rotations into at most 6 permutations
 
 def swap_and_add_seq(nums, seq):  # at most 1.5N swaps
     nums[-1], nums[-2] = nums[-2], nums[-1]
@@ -59,7 +59,7 @@ def sorting_permutation_unit():
         while True:
             # rotate the first N-1 ones into the correct positions and swap(N-1, N)
             # until Nth position becomes the largest one,
-            # at most 4N operations
+            # at most 6N operations
             while A[-1] != len(A)-1:
                 rotate_and_add_seq(A, (len(A)-2) - (shift[0]+A[-1])%(len(A)-1), seq, shift)
                 swap_and_add_seq(A, seq)
@@ -80,25 +80,17 @@ def sorting_permutation_unit():
         result.append(" ".join(map(str, seq)))
     return "\n".join(result)
 
-ROTATE_BY = [1, 3, 12, 20]
+ROTATE_BY = [1, 3, 9, 27]
 ROTATIONS = []
 MAX_N = 50
-dp = [None for _ in xrange(len(ROTATE_BY)*ROTATE_BY[-1]+1)]
-dp[0] = []
-for i in xrange(1, len(dp)):
-    for j in xrange(len(ROTATE_BY)):
-        if i-ROTATE_BY[j] < 0 or dp[i-ROTATE_BY[j]] is None:
-            continue
-        if dp[i] is None or len(dp[i-ROTATE_BY[j]])+1 < len(dp[i]):
-            dp[i] = list(dp[i-ROTATE_BY[j]])
-            dp[i].append(j+2)  # 1-based index and index 1 reserved for swap permutation
 for k in xrange(MAX_N-1):
-    ROTATIONS.append(dp[k])
-    count = len(ROTATIONS[-1])
-    for i in xrange(k+MAX_N-1, len(dp), MAX_N-1):
-        if len(dp[i]) < len(ROTATIONS[-1]):
-            ROTATIONS[-1] = dp[i]
-            count = len(ROTATIONS[-1])
-    assert(count <= 4)  # each rotations could be represented as at most 4 permutations
+    count = 0
+    ROTATIONS.append([])
+    for i in reversed(xrange(len(ROTATE_BY))):
+        q, k = divmod(k, ROTATE_BY[i])
+        if q > 0:
+            ROTATIONS[-1].extend([i+2]*q)  # 1-based index and index 1 reserved for swap permutation
+            count += q
+    assert(count <= 6)  # each rotations could be represented as at most 6 permutations
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, sorting_permutation_unit())
