9125e2f2e694b106fc66b702e605ff58ff268319
Allen Liu <kamyu104@gmail.com>
2019-08-18 23:52:20 +0800

    update

--- World Finals/wont-sum-must-now.py
+++ World Finals/wont-sum-must-now.py
@@ -31,49 +31,57 @@ def palindromes(S):
                 yield p
         n *= 10
 
-def find_pair_with_same_length(s, x, y, start, carry, right_carry, prev_right_carry=0):
-    #print "--"*3, start, carry, right_carry
-    if 2 * start >= len(s):
-        if len(s)%2:
-            return carry == prev_right_carry
-        #print "end", carry, right_carry
+def find_pair_with_same_length(s, x, y, start, carry, right_carry):
+    if start == len(x)-1-start+1:  # terminated statement of even length
         return carry == right_carry
-    for i in xrange(10):
-        for need_carry in xrange(2):
+    if start == len(x)-1-start:  # terminated statement of odd length
+        for i in xrange(10):
             if start == 0 and i == 0:
                 continue
-            target = s[-1-start] + 10*carry - need_carry
+            target = s[len(x)-1-start] + 10*carry - right_carry
             j = target-i
             if not (0 <= j <= 9):
                 continue
             if start == 0 and j == 0:
                 continue
-            #print start, s[start], right_carry, target2, i, j
-            if s[start] != (i+j+right_carry)%10:
-                # print s, x, y, start, len(s), i, j
-                continue
-            #print start, "target2", target2, "=", i, "+", j, "+", need_carry, right_carry
-            new_carry_from_right = (i+j+right_carry)//10
-            #print "carry", new_carry_from_right, right_carry, x, y
-            #print "xx", i, j, new_carry_from_right
             x[start], x[-1-start] = i, i
             y[start], y[-1-start] = j, j
-            #print start, "target", target, "=", i, "+", j, "+", need_carry, right_carry
-            if find_pair_with_same_length(s, x, y, start+1, need_carry, new_carry_from_right, right_carry):
+            return True
+        return False
+    
+    for i in xrange(10):
+        for new_carry in xrange(2):
+            if start == 0 and i == 0:
+                continue
+            target = s[len(x)-1-start] + 10*carry - new_carry
+            j = target-i
+            if not (0 <= j <= 9):
+                continue
+            if start == 0 and j == 0:
+                continue
+            if s[start] != (i+j+right_carry)%10:
+                continue
+            x[start], x[-1-start] = i, i
+            y[start], y[-1-start] = j, j
+            if find_pair_with_same_length(s, x, y, start+1, new_carry, (i+j+right_carry)//10):
                 return True
             y[start], y[-1-start] = None, None
             x[start], x[-1-start] = None, None
 
+def get_overhang(s, start, overhang):
+    return int("".join(map(str, s[-(start+overhang):-start])))
+
 def find_pair_with_hangover_length(s, x, y, start, carry, right_carry):
+    return False
+    overhang = len(x)-len(y)
+    get_overhang(s, start, overhang)
     return False
 
 def find_pair(S, i, j, carry):
     s = map(int, list(str(S)))
     s.reverse()
-    if carry:
-        s.pop()      
     x, y = [None]*i, [None]*j
-    if len(x) == len(y):
+    if i == j:
         result = find_pair_with_same_length(s, x, y, 0, carry, 0)
     else:
         result = find_pair_with_hangover_length(s, x, y, 0, carry, 0)
@@ -110,7 +118,7 @@ def wont_sum_must_now():
                     if p == 0:
                         return "%d %d" % (result[0], result[1])
                     return "%d %d %d" % (p, result[0], result[1])
-    assert(False)
+    #assert(False)
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, wont_sum_must_now())
