8b3adad31de19228e7c32ac634100e032a4a00ad
Allen Liu <kamyu104@gmail.com>
2019-08-20 07:55:16 +0800

    update

--- World Finals/wont-sum-must-now.py
+++ World Finals/wont-sum-must-now.py
@@ -10,8 +10,8 @@
 def to_int(x):  # list of ints to int
     return int("".join(map(str, x)))
 
-def to_list(X):  # string to list of ints
-    return map(int, list(X))
+def to_list(X):  # int to list of ints
+    return map(int, list(str(X)))
 
 def gen_palindromes(S):
     # at most 208 times because the smallest palindrome of triples
@@ -84,7 +84,7 @@ def find_pair_with_overhang_length(s, x, y, start, left_carry, right_carry, left
                 left_carry*(10**overhang) - new_left_carry - left_Y
         if not (0 <= left_X < 10**overhang):
             return None
-        left_x = to_list(str(left_X))
+        left_x = to_list(left_X)
         left_x = [0]*(overhang-len(left_x)) + left_x  # 0-padding
         if start == 0 and left_x[0] == 0:  # leading digit can't be 0
             return None
@@ -93,21 +93,22 @@ def find_pair_with_overhang_length(s, x, y, start, left_carry, right_carry, left
             return None
         return left_x
 
-    def find_right_y():
+    def find_left_y():
         right_y_len = min(len(y)-start*2, overhang)
         right_S = to_int(s[start:start+right_y_len][::-1])
         right_X = to_int(left_x[:right_y_len][::-1])
         new_right_carry, right_Y = map(abs, divmod(right_S-right_X-right_carry, 10**right_y_len))
-        right_y = to_list(str(right_Y)[::-1])
-        right_y = right_y + [0]*(right_y_len-len(right_y))  # 0-padding
-        if start == 0 and right_y[0] == 0:  # leading digit can't be 0
+        right_y = to_list(right_Y)
+        right_y = [0]*(right_y_len-len(right_y)) + right_y  # 0-padding
+        left_y = right_y[::-1]
+        if start == 0 and left_y[0] == 0:  # leading digit can't be 0
             clear_digits(x, left_x, start)
             return None, None
-        if not set_digits(y, right_y, start):
-            clear_digits(y, right_y, start)
+        if not set_digits(y, left_y, start):
+            clear_digits(y, left_y, start)
             clear_digits(x, left_x, start)
             return None, None
-        return right_y, new_right_carry
+        return left_y, new_right_carry
 
     if len(x)-start*2 <= 0:
         return left_carry == right_carry
@@ -116,18 +117,18 @@ def find_pair_with_overhang_length(s, x, y, start, left_carry, right_carry, left
         left_x = find_left_x()
         if left_x is None:
             continue
-        right_y, new_right_carry = [], right_carry  # pass current right carry if y is not updated
+        left_y, new_right_carry = [], right_carry  # pass current right carry if y is not updated
         new_left_Y = 0
         if len(y)-start*2 > 0:  # if y needs update
-            right_y, new_right_carry = find_right_y()
-            if right_y is None or new_right_carry is None:
+            left_y, new_right_carry = find_left_y()
+            if left_y is None or new_right_carry is None:
                 continue
             if len(y)-start*2 > overhang:  # find left y to be updated
-                new_left_Y = to_int(right_y[:(len(y)-start*2)-overhang])
+                new_left_Y = to_int(left_y[:(len(y)-start*2)-overhang])
         if find_pair_with_overhang_length(s, x, y, start+overhang,
                                           new_left_carry, new_right_carry, new_left_Y):
             return True
-        clear_digits(y, right_y, start)
+        clear_digits(y, left_y, start)
         clear_digits(x, left_x, start)
     return False
 
@@ -146,11 +147,11 @@ def find_pair(s, i, j, left_carry):
 def wont_sum_must_now():
     S = input()
 
-    s = to_list(str(S))
+    s = to_list(S)
     if s == s[::-1]:
         return S
     for P in gen_palindromes(S):
-        s = to_list(str(S-P))
+        s = to_list(S-P)
         s.reverse()
         for i in xrange(len(s)-1, len(s)+1):
             left_carry = 0
